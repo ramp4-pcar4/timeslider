8056f0d8b2e3e77cab95b428129cc914c065c68d
Spencer Wahl <spencer.s.wahl@gmail.com>
2026-05-27 16:06:25 -0400

    appbar button, header layout

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,104 @@
 <template>
-    <div ref="el" class="time-slider shadow-tm absolute w-full h-full left-0 flex flex-col items-center bg-white">
-        <button
-            class="absolute play-button"
-            @click="intervalID >= 0 ? endLoop() : startLoop()"
-            :content="$t(intervalID >= 0 ? 'timeslider.pause' : 'timeslider.play')"
-            v-tippy="{ placement: 'top', hideOnClick: false, animateFill: true, theme: 'ramp4' }"
-            :aria-label="$t(intervalID >= 0 ? 'timeslider.pause' : 'timeslider.play')"
-        >
-            <svg
-                v-if="intervalID === -1"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#595959"
+    <div
+        v-show="open"
+        ref="el"
+        class="time-slider shadow-tm absolute w-full h-full left-0 flex flex-col items-center bg-white"
+    >
+        <div class="time-slider-header h-44 w-full grid grid-cols-3">
+            <div class="flex items-center">
+                <button
+                    class="ml-10"
+                    @click="intervalID >= 0 ? endLoop() : startLoop()"
+                    :content="$t(intervalID >= 0 ? 'timeslider.pause' : 'timeslider.play')"
+                    v-tippy="{ placement: 'top', hideOnClick: false, animateFill: true, theme: 'ramp4' }"
+                    :aria-label="$t(intervalID >= 0 ? 'timeslider.pause' : 'timeslider.play')"
+                >
+                    <svg
+                        v-if="intervalID === -1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#595959"
+                    >
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path d="M8 5v14l11-7z" />
+                    </svg>
+                    <svg
+                        v-else
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#595959"
+                    >
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+                    </svg>
+                </button>
+            </div>
+            <span class="my-2.5 text-base range-display justify-self-center"
+                ><span class="">{{ displayFormat ? (displayFormat as any).to(parseInt(range[0]), 0) : range[0] }}</span
+                ><span class="" v-if="range[1]">
+                    - {{ displayFormat ? (displayFormat as any).to(parseInt(range[1]), 1) : range[1] }}</span
+                ></span
             >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M8 5v14l11-7z" />
-            </svg>
-            <svg
-                v-else
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#595959"
-            >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
-            </svg>
-        </button>
-        <span class="my-2.5 text-base range-display"
-            ><span class="">{{ displayFormat ? (displayFormat as any).to(parseInt(range[0]), 0) : range[0] }}</span
-            ><span class="" v-if="range[1]">
-                - {{ displayFormat ? (displayFormat as any).to(parseInt(range[1]), 1) : range[1] }}</span
-            ></span
-        >
-        <button
-            class="absolute minimize-button"
-            @click="minimizeToggle()"
-            :content="$t(minimized ? 'timeslider.expand' : 'timeslider.minimize')"
-            v-tippy="{ placement: 'top', hideOnClick: false, animateFill: true, theme: 'ramp4' }"
-            :aria-label="$t(minimized ? 'timeslider.expand' : 'timeslider.minimize')"
-        >
-            <svg
-                v-if="!minimized"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="6 6 36 36"
-                height="24"
-                width="24"
-                fill="#595959"
-            >
-                <path d="m24 30.75-12-12 2.15-2.15L24 26.5l9.85-9.85L36 18.8Z" />
-            </svg>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="6 6 36 36" fill="#595959">
-                <path d="M14.15 30.75 12 28.6l12-12 12 11.95-2.15 2.15L24 20.85Z" />
-            </svg>
-        </button>
+            <div class="flex items-center">
+                <button
+                    class="ml-auto"
+                    @click="minimizeToggle()"
+                    :content="$t(minimized ? 'timeslider.expand' : 'timeslider.minimize')"
+                    v-tippy="{ placement: 'top', hideOnClick: false, animateFill: true, theme: 'ramp4' }"
+                    :aria-label="$t(minimized ? 'timeslider.expand' : 'timeslider.minimize')"
+                >
+                    <svg
+                        v-if="!minimized"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="6 6 36 36"
+                        height="24"
+                        width="24"
+                        fill="#595959"
+                    >
+                        <path d="m24 30.75-12-12 2.15-2.15L24 26.5l9.85-9.85L36 18.8Z" />
+                    </svg>
+                    <svg
+                        v-else
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24"
+                        width="24"
+                        viewBox="6 6 36 36"
+                        fill="#595959"
+                    >
+                        <path d="M14.15 30.75 12 28.6l12-12 12 11.95-2.15 2.15L24 20.85Z" />
+                    </svg>
+                </button>
+                <button
+                    class="mr-10"
+                    @click="open = false"
+                    :content="$t('timeslider.close')"
+                    v-tippy="{
+                        placement: 'top',
+                        hideOnClick: true,
+                        animateFill: true,
+                        theme: 'ramp4',
+                        appendTo: 'parent'
+                    }"
+                    :aria-label="$t('timeslider.close')"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 -960 960 960"
+                        width="24px"
+                        fill="#595959"
+                    >
+                        <path
+                            d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </div>
         <div class="time-slider-backdrop"></div>
         <div v-show="!minimized" ref="sliderTarget" class="noUi-target noUiSlider"></div>
     </div>
@@ -97,6 +139,7 @@ const sliderElement = ref<HTMLElement>();
 const slider = ref<API>();
 const range = ref<string[]>(['', '']);
 const intervalID = ref(-1);
+const open = ref<boolean>(true);
 
 const displayFormat = ref<Formatter | undefined>();
 let internalFormat: Formatter | undefined;
@@ -143,6 +186,10 @@ onMounted(() => {
         if (props.rInstance.geo.layer.allLayers().every((l: any) => l.isLoaded)) {
             sliderUpdateHandler();
         }
+    });
+
+    props.rInstance.event.on('timeslider/toggle', () => {
+        open.value = !open.value;
     });
 });
 
@@ -386,7 +433,7 @@ const createFormat = (formatter: TimeSliderFormatter | undefined): Formatter | u
     z-index: 5;
 }
 .time-slider-container.minimized {
-    height: 50px;
+    height: 44px;
 }
 
 .time-slider {
@@ -401,19 +448,6 @@ const createFormat = (formatter: TimeSliderFormatter | undefined): Formatter | u
         color: #555;
     }
 
-    .play-button,
-    .minimize-button {
-        top: 10px;
-    }
-
-    .play-button {
-        left: 16px;
-    }
-
-    .minimize-button {
-        right: 16px;
-    }
-
     // MEDIA QUERY
     @media screen and (max-width: 640px) {
         .noUi-value:nth-of-type(4n) {
@@ -426,14 +460,6 @@ const createFormat = (formatter: TimeSliderFormatter | undefined): Formatter | u
 
         .noUi-marker-large {
             height: 9px !important;
-        }
-
-        .play-button {
-            @apply left-2 top-0;
-        }
-
-        .minimize-button {
-            @apply right-2 top-0;
         }
     }
 
